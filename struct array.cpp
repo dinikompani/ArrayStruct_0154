014367fc17b57139186bb4716d1af83390eeb904
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2025-04-20 16:40:37 +0700

    menambahkan fitur menampilkan data array mahasiswa

--- struct array.cpp
+++ struct array.cpp
@@ -22,4 +22,15 @@ int main()
         cout << "Masukkan alamat = ";
         cin >> mhs[i].alamat;
     }
+
+    cout << endl;
+    cout << "Menampilkan Data" << endl;
+    cout << endl;
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "Mahasiswa ke-" << i + 1 << endl;
+        cout << "NIM = " << mhs[i].nim << endl;
+        cout << "Nama = " << mhs[i].nama << endl;
+        cout << "Alamat = " << mhs[i].alamat << endl;
+    }
 }

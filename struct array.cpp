37036597366cd8c30abaeb7c3739b760fa24be48
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2025-04-20 16:31:03 +0700

    Implementasi array dari struct mahasiswa

--- struct array.cpp
+++ struct array.cpp
@@ -7,3 +7,19 @@ struct Mahasiswa
     string nim;
     string alamat;
 };
+
+int main()
+{
+    //Membuat object struct dalam array
+    Mahasiswa mhs[4];
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "Mahasiswa ke-" << i + 1 << endl;
+        cout << "Masukkan nim = ";
+        cin >> mhs[i].nim;
+        cout << "Masukkan nama = ";
+        cin >> mhs[i].nama;
+        cout << "Masukkan alamat = ";
+        cin >> mhs[i].alamat;
+    }
+}

8b83fbc7bdd3092b347781aea45b110bb14e2143
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2025-04-20 15:39:18 +0700

    Menambahkan fungsi main

--- struct.cpp
+++ struct.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 using namespace std;
 
-struct Mahaiswa 
+struct Mahasiswa 
 {
     string nama;
     string nim;
     string alamat;
+};
+
+int main()
+{
+    // Membuat object struct
+    Mahasiswa mhs;
+
+    cout << "Masukkan nim = ";
+    cin >> mhs.nim;
+    cout << "Masukkan nama = ";
+    cin >> mhs.nama;
+    cout << "Masukkan alamat = ";
+    cin >> mhs.alamat;
 };

43442bb3581c5adbae093122848747378fa5472c
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2025-04-20 17:33:48 +0700

    menambahkan object dalam array

--- nested struct.cpp
+++ nested struct.cpp
@@ -13,3 +13,21 @@ struct Mahasiswa
     string nim;
     DetailAlamat alamat;
 };
+
+int main()
+{
+    //Membuat object struct dalam array
+    Mahasiswa mhs[2];
+    for (int i = 0; i < 2; i++)
+    {
+        cout << "Mahasiswa ke-" << i + 1 << endl;
+        cout << "Masukkan nim = ";
+        cin >> mhs[i].nim;
+        cout << "Masukkan nama = ";
+        cin >> mhs[i].nama;
+        cout << "Masukkan kota = ";
+        cin >> mhs[i].alamat.kota;
+        cout << "Masukkan provinsi = ";
+        cin >> mhs[i].alamat.provinsi;
+    }
+}

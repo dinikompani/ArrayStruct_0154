538c8e11440b4ab3d73143a04c9b01ce91845a24
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2025-04-20 17:16:05 +0700

    membuat object struct dalam array

---
+++ struct getline.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat
+{
+    string kota;
+    string provinsi;
+};
+
+struct Mahasiswa
+{
+    string nama;
+    string nim;
+    DetailAlamat alamat;
+};
+
+int main()
+{
+    // Membuat object struct dalam array
+    Mahasiswa mhs[2];
+    for (int i = 0; i < 2; i++)
+    {
+        cout << "Mahasiswa ke-" << i + 1 << endl;
+        cout << "Masukkan nim = ";
+        cin >> mhs[i].nim;
+        cin.ignore();
+        cout << "Masukkan nama = ";
+        getline(cin, mhs[i].nama);
+        cout << "Masukkan kota = ";
+        cin >> mhs[i].alamat.kota;
+        cout << "Masukkan provinsi = ";
+        cin >> mhs[i].alamat.provinsi;
+        cin.ignore();
+
+    }
+}

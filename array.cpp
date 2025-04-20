60c607fbce0270737646c1e3275ef4bc3246f1c3
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2025-04-20 16:07:16 +0700

    implementasi fugsi main

--- array.cpp
+++ array.cpp
@@ -7,5 +7,13 @@ int nilai[] = {1, 2, 4, 7};
 // deklarasi array dengan ukuran dan diberi nilai awal
 int nilaiDua [3] = {1, 2, 3};
 
-// deklarasi array dengan ukuran, tanpa nilai awal 
+// deklarasi array dengan ukuran, tanpa nilai awal
 string nama[4];
+
+int main()
+{
+    cout << "Nilai array ke 3 = " << nilai[2] << endl;
+    nilai[2] = 100;
+    cout << "Nilai array ke 3 = " << nilai[2] << endl;
+
+}

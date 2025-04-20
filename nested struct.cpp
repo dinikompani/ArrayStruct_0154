979bc8161756db3b7e235d846e65127987d0a8f6
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2025-04-20 17:40:21 +0700

    menambahkan input data mahasiswa

--- nested struct.cpp
+++ nested struct.cpp
@@ -30,4 +30,16 @@ int main()
         cout << "Masukkan provinsi = ";
         cin >> mhs[i].alamat.provinsi;
     }
+
+    cout << endl;
+    cout << "Menampilkan Data" << endl;
+    cout << endl;
+    for (int i = 0; i < 2; i++)
+    {
+        cout << "Mahasiswa ke-" << i + 1 << endl;
+        cout << "NIM = " << mhs[i].nim << endl;
+        cout << "Nama = " << mhs[i].nama << endl;
+        cout << "provinsi = " << mhs[i].alamat.provinsi << endl;
+
+    }
 }
